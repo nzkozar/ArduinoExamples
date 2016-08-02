ecf65b8280ef8e6f6bfe38e46f4ba291462b8be2
nzkozar <nzkozar@users.noreply.github.com>
2016-08-03 00:21:14 +0200

    Add files via upload

---
+++ esp8266_WIFI_connect/esp8266_WIFI_connect.ino
@@ -0,0 +1,31 @@
+#include "ESP8266WiFi.h"
+const char* ssid = "<ssid>";
+const char* password = "<password>";   
+
+WiFiClient wifiClient;
+
+void setup(){
+  pinMode(BUILTIN_LED, OUTPUT);
+  
+  Serial.begin(115200);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+                                    // but actually the LED is on; this is because 
+                                    // it is acive low on the ESP-01)
+  delay(1000);                      // Wait for a second
+  digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(1000);
+}
